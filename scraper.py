f607707333b161d4519ffd88441380c4d0518556
Jamie <jnjoroge141@gmail.com>
2019-12-11 15:52:20 +0300

    back

--- scraper.py
+++ scraper.py
@@ -3,10 +3,10 @@ from bs4 import BeautifulSoup
 import smtplib
 import time
 
-URL = 'INSERT PRODUCT URL'
+URL = 'https://www.amazon.com/HP-1080p-Intel-i5-8265U-Windows/dp/B07QZNWH3W/ref=pd_sbs_147_1/133-7321397-2365158?_encoding=UTF8&pd_rd_i=B07QZNWH3W&pd_rd_r=eba4f24d-ab01-46eb-8627-604fbad19067&pd_rd_w=o9LNu&pd_rd_wg=TQVoj&pf_rd_p=5873ae95-9063-4a23-9b7e-eafa738c2269&pf_rd_r=ZZQ5GBVYWNT844A5FJEV&psc=1&refRID=ZZQ5GBVYWNT844A5FJEV'
 
 #get this by typing "my user agent on your browser"
-headers = {"User-Agent": 'USER AGENT AS FROM THE BROWSER'}
+headers = {"User-Agent": 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0'}
 
 def checkPrice():
     page = requests.get(URL, headers=headers)
@@ -28,7 +28,7 @@ def send_email():
     server.ehlo()
 
 #you may need to allow less secure app from google
-    server.login('from_email', 'password')
+    server.login('jamesnjoroge141@gmail.com', 'Traditional.?')
 
     Subject = 'Alert! Price dropped'
     body = 'check price for hp envy 13 at: PRODUCT URL'
@@ -36,8 +36,8 @@ def send_email():
     msg = f"Subject: {Subject}\n\n  Body: {body}"
 
     server.sendmail(
-        'from_email',
-        'to_email',
+        'jamesnjoroge141@gmail.com',
+        'jnjoroge141@gmail.com',
         msg
         )
     print('Email sent')
